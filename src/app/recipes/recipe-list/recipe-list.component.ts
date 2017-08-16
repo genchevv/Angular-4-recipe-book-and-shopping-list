b8c1628b5c3cb790c42659feecfbb7decc85388b
Radoslav Genchev <r.genchew@gmail.com>
2017-08-16 16:57:41 +0200

    Added some dummy recipes and displayed them properly on the recipe-list view page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-	recipes = [];
+	recipes: Recipe[] = [
+		new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Chicken-kathi-roll-recipe.jpg/640px-Chicken-kathi-roll-recipe.jpg'),
+		new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_960_720.jpg'),
+		new Recipe('A Test Recipe 3', 'This is simply a test 3', 'https://static.pexels.com/photos/23086/food-restaurant-kitchen-meat-23086.jpg')
+	];
 
   constructor() { }
 
