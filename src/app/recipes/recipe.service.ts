b677dbb678fbcfce2c5dd10251dd810834fd9bc3
Radoslav Genchev <r.genchew@gmail.com>
2017-08-17 21:17:46 +0200

    Used Recipe Service for Cross-Component Communication and removed the unused code

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+	recipeSelected = new EventEmitter<Recipe>();
+
 	private recipes: Recipe[] = [
 		new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Chicken-kathi-roll-recipe.jpg/640px-Chicken-kathi-roll-recipe.jpg'),
 		new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_960_720.jpg'),
