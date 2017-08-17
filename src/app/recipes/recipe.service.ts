527347ce85cfba24a7a1d815020adc3d49a6f098
Radoslav Genchev <r.genchew@gmail.com>
2017-08-17 21:48:11 +0200

    Added Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,28 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 
 	private recipes: Recipe[] = [
-		new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Chicken-kathi-roll-recipe.jpg/640px-Chicken-kathi-roll-recipe.jpg'),
-		new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_960_720.jpg'),
-		new Recipe('A Test Recipe 3', 'This is simply a test 3', 'https://static.pexels.com/photos/23086/food-restaurant-kitchen-meat-23086.jpg')
+		new Recipe(
+			'Tasty Schnitzel', 
+			'This is simply a test 1', 
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Chicken-kathi-roll-recipe.jpg/640px-Chicken-kathi-roll-recipe.jpg',
+			[
+				new Ingredient('Meat', 1),
+				new Ingredient('French Fries', 20)
+			]),
+		new Recipe(
+			'Big Fat Burger', 
+			'This is simply a test 2', 
+			'https://static.pexels.com/photos/23086/food-restaurant-kitchen-meat-23086.jpg',
+			[
+				new Ingredient('Buns', 2),
+				new Ingredient('Meat', 1)
+			])
 	];
 
 	getRecipes() {
